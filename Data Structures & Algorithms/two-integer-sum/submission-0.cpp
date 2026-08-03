c6cf0b4fb1e9382b0a785dbc44be654a7c00b105
Rajat Kumar Upadhyay <59369198+rajatupadhyay9@users.noreply.github.com>
2026-08-03 22:23:26 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> nums_hash;
+
+        for(int idx = 1; auto n : nums) {
+            if(nums_hash[target-n] > 0)
+                return {nums_hash[target-n] - 1, idx - 1};
+            nums_hash[n] = idx;
+            idx++;
+        }
+        return {-1, -1};
+    }
+};
